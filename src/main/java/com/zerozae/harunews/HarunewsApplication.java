ed3aeb9a6c5664360c29215a11bb5bf24b5c58b1
park0jae <okvv26@naver.com>
2024-01-20 14:13:55 +0900

    initial commit & H2 DB Setting

---
+++ src/main/java/com/zerozae/harunews/HarunewsApplication.java
@@ -0,0 +1,13 @@
+package com.zerozae.harunews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarunewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarunewsApplication.class, args);
+	}
+
+}
